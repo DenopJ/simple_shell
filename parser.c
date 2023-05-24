56d08dcdd9d808275b442c0ecfcab025acdeb77f
Emmanuel Adjei <emmanueladjeikrop@gmail.com>
2023-05-23 23:36:42 -0700

    parser

---
+++ parser.c
@@ -0,0 +1,85 @@
+#include "shell.h"
+
+/**
+ * ks_cmd - determines if the file is an executable command
+ * @info: the info struct
+ * @path: path to a file
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int ks_cmd(info_t *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * dup_char - duplicates a character
+ * @pathstr: the PATH string
+ * @start: starting index
+ * @stop: stopping index
+ * Return: pointer to new buffer
+ */
+char *dup_char(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int a = 0, c = 0;
+
+	for (c = 0, a = start; a < stop; a++)
+		if (pathstr[a] != ':')
+			buf[c++] = pathstr[a];
+	buf[c] = 0;
+	return (buf);
+}
+
+/**
+ * fnd_pth - find the cmd in the PATH string
+ * @info: the info struct
+ * @pathstr: the PATH string
+ * @cmd: the cmd to find
+ *
+ * Return: full path of cmd if found or NULL
+ */
+char *fnd_pth(info_t *info, char *pathstr, char *cmd)
+{
+	int a = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (ks_cmd(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[a] || pathstr[a] == ':')
+		{
+			path = dup_char(pathstr, curr_pos, a);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (ks_cmd(info, path))
+				return (path);
+			if (!pathstr[a])
+				break;
+			curr_pos = a;
+		}
+		a++;
+	}
+	return (NULL);
+}
